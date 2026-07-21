4acef16056514771f38b43beec0c6cae1b4792fb
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-07-21 12:40:47 +0530

    Implemented search in rotated sorted array

---
+++ dsa-journey/03-practice/leetcode/LC0033_search_in_rotated_sorted_array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=(int)nums.size();
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid=l+((h-l)/2);
+            if(nums[mid]==target) return mid;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target&&target<nums[mid]){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(target>nums[mid]&&target<=nums[h]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};

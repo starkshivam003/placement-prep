4773225b08f7c89323d0a2b8a2f3b73f7e87719c
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-06 11:05:55 +0530

    Add solution for deleting a node in a linked list
    Implemented a solution to delete a node from a singly-linked list by copying the value of the next node and adjusting pointers.

---
+++ dsa-journey/03-practice/leetcode/LC0237_delete_node_in_a_linked_list.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* p;
+        p=node;
+        while(p->next->next){
+            p->val=p->next->val;
+            p=p->next;
+        }
+        p->val=p->next->val;
+        p->next=NULL;
+    }
+};

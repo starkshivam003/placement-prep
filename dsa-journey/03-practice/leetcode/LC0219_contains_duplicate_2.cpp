867cd2ec37aa7f4a2b11f4416a51c2d90522cc67
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-08 11:23:50 +0530

    Implement containsNearbyDuplicate function

---
+++ dsa-journey/03-practice/leetcode/LC0219_contains_duplicate_2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> window;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (window.count(nums[i])) {
+                return true;
+            }
+            window.insert(nums[i]);
+            if ((int)window.size() > k) {
+                window.erase(nums[i - k]);
+            }
+        }
+        return false;
+    }
+};

cf6c37e10d7d1dd9c745ca5996d23af9eae4fea3
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-10 10:14:19 +0530

    Optimize palindrome linked list solution
    Refactor palindrome check to use O(1) space complexity.

--- dsa-journey/03-practice/leetcode/LC0234_palindrome_linked_list.cpp
+++ dsa-journey/03-practice/leetcode/LC0234_palindrome_linked_list.cpp
@@ -40,7 +40,7 @@ public:
     }
 };*/
 //attempt 2: TC-O(n) and SC-O(n)
-class Solution{
+/*class Solution{
 private:
     vector<int> stk;
     int top;
@@ -101,26 +101,40 @@ public:
         }
         return true;
     }
-};
+};*/
 //attempt 3: TC-O(n) and SC-O(1)
-/*class Solution(){
+class Solution {
 public:
     bool isPalindrome(ListNode* head){
         if(head->next==NULL){
             return true;
         }
-        ListNode *p,*q,*halfHead*r;
+        ListNode *p,*q,*halfHead,*r;
         p=head->next;
         q=head;
-        while(p->next){
+        while(p&&p->next){
             p=p->next->next;
             q=q->next;
         }
-        q=q->next;
-        halfHead=q;
-        r=q;
-        while(q->next){
-
+        halfHead=q->next;
+        q->next=NULL;
+        q=halfHead->next;
+        halfHead->next=NULL;
+        while(q){
+            r=q;
+            q=q->next;
+            r->next=halfHead;
+            halfHead=r;
         }
+        p=head;
+        q=halfHead;
+        while(p&&q){
+            if(p->val!=q->val){
+                return false;
+            }
+            p=p->next;
+            q=q->next;
+        }
+        return true;
     }
-}:*/
+};

36cf15b74e9f196ba3a1d4cf6bafb8464c255f37
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-10 11:20:02 +0530

    Implement single pass bit shift method for conversion
    Refactor the solution to use a single pass with bit shifting to convert a binary number in a linked list to an integer.

--- dsa-journey/03-practice/leetcode/LC1290_convert_binary_number_in_a_linked_list_to_integer.cpp
+++ dsa-journey/03-practice/leetcode/LC1290_convert_binary_number_in_a_linked_list_to_integer.cpp
@@ -8,7 +8,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+
+ // all the solutions take O(n) time and O(1) space
+
+ //attmept 1: two pass
+/*class Solution {
 public:
     int getDecimalValue(ListNode* head) {
         if(head->next==NULL){
@@ -29,6 +33,34 @@ public:
         while(p){
             value+=((p->val)*i);
             i*=2;
+            p=p->next;
+        }
+        return value;
+    }
+};*/
+//attempt 2: single pass, bit shift method 1
+/*class Solution{
+public:
+    int getDecimalValue(ListNode* head){
+        ListNode *p;
+        p=head;
+        int value=0;
+        while(p){
+            value=(value*2)+(p->val);
+            p=p->next;
+        }
+        return value;
+    }
+};*/
+//attempt 3: single pass, bit shift Method 2
+class Solution{
+public:
+    int getDecimalValue(ListNode* head){
+        ListNode *p;
+        p=head;
+        int value=0;
+        while(p){
+            value=((value<<1)|(p->val));
             p=p->next;
         }
         return value;

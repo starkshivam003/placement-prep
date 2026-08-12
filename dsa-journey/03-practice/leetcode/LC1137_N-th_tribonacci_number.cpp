6b169b859f4743e65e931e2f888277f839b80bb3
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-12 09:43:56 +0530

    Optimize tribonacci calculation with static array
    Refactored tribonacci function to use a static array for improved efficiency.

--- dsa-journey/03-practice/leetcode/LC1137_N-th_tribonacci_number.cpp
+++ dsa-journey/03-practice/leetcode/LC1137_N-th_tribonacci_number.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//attempt 1: using recursion and meomization
+/*class Solution {
 public:
     int A[38]={0};
     int tribonacci(int n) {
@@ -8,6 +9,22 @@ public:
             return A[n];
         }
         A[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return A[n];
+    }
+};*/
+//attempt 2: using a static array
+class Solution {
+public:
+    int tribonacci(int n) {
+        static int A[38];
+        static bool built = false;
+        if(!built){
+            A[0]=0; A[1]=1; A[2]=1;
+            for(int i=3; i<38; i++){
+                A[i] = A[i-1]+A[i-2]+A[i-3];
+            }
+            built = true;
+        }
         return A[n];
     }
 };

45a8b4b714117752da1d84ae5670f202bb284bdc
Shivam Kumar <132679055+starkshivam003@users.noreply.github.com>
2026-08-14 11:50:34 +0530

    Implement isSubsequence function in LC0392

---
+++ dsa-journey/03-practice/leetcode/LC0392_is_subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=(int)s.size();
+        int m=(int)t.size();
+        if(n>m){
+            return false;
+        }
+        if(n==m){
+            for(int i=0;i<n;i++){
+                if(s[i]!=t[i]){
+                    return false;
+                }
+            }
+        }
+        int j=0;
+        for(int i=0;i<n;i++){
+            int flag=0;
+            while(j<m){
+                if(t[j]==s[i]){
+                    flag=1;
+                    j++;
+                    break;
+                }
+                j++;
+            }
+            if(flag==0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
